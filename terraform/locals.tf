4dc8741d38b12d2e4210bf1096739e3d13daec5a
Aboubakr2000Cloud <aboubacre.ijannane@gmail.com>
2026-07-20 15:55:33 +0100

    delete duplicate

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,6 +1,6 @@
 locals {
   name_prefix    = "weather-security-${var.environment}"
-  monitor_prefix = "ws"-"${var.environment}"
+  monitor_prefix = "ws-${var.environment}"
   common_tags    = {
     Project      = "ecs-weather-platform-secured"
     Environment  = var.environment
